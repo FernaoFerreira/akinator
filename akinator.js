9464d16449671f73ec4a5a2d7f43a394d0e3054f
FernaoFerreira <fernaoqf@gmail.com>
2025-11-13 15:42:24 -0300

    implemented escolherMelhorPergunta

--- akinator.js
+++ akinator.js
@@ -115,8 +115,76 @@ const candidatos = [
     }
 ];
 
+const linguagemNatural = {
+    eHumano : 'é humano',
+    usaOculos : "usa óculos",
+    eAdulto : "é adulto",
+    usaMagia : "usa magia",
+    apareceEmFilme : "aparece em filme",
+    usaArmaBranca : "usa arma branca",
+    temCabeloLoiro : "tem cabelo loiro"
+}
 //Cria as variáveis de estado
 
 let candidatosRestantes = [...candidatos];
 let perguntasFeitas = [];
 
+const todasAsPerguntas = Object.keys(candidatos[0].atributos);
+
+//cria um array de perguntas disponiveis
+
+function escolherMelhorPergunta(perguntasFeitas, candidatosRestantes){
+    //pega todas as perguntas que ainda nao foram feitas
+    const perguntasDisponiveis = todasAsPerguntas.filter(pergunta => !perguntasFeitas.includes(pergunta));
+
+    //verifica se o jogo pode continuar
+    if(perguntasDisponiveis.length === 0){
+        return null;
+    }
+
+    let melhorPergunta = null;
+    let melhorScore = Infinity;
+
+    //testa as perguntas
+    perguntasDisponiveis.forEach(pergunta =>{
+        const comAtributo = candidatosRestantes.filter(
+            c => c.atributos[pergunta] === true
+        ).length;
+
+        const semAtributo = candidatosRestantes.length - comAtributo;
+        
+        const score = Math.abs(comAtributo - semAtributo);
+        
+        if(score < melhorScore){
+            melhorScore = score;
+            melhorPergunta = pergunta;
+        }
+    });
+
+    return melhorPergunta;
+    
+}
+
+
+
+function playGame(){
+
+    //casos base
+    if(candidatosRestantes.length === 1){
+        console.log("é fulano?"); //como eu faço pra ele retornar o nome do personagem
+    }
+    if(candidatosRestantes === 0){
+        console.log("Você me venceu, gostaria de registrar o nome do personagem?");
+    }
+
+    //como eu faço pra ele imprimir a melhor pergunta
+
+    const pergunta = escolherMelhorPergunta();
+    //tenho que fazer o akinator desclassificar os personagens dependendo da resposta pra arvore de decisao funcionar
+    if(btnNao.addEventListener("click", () =>{
+        //logica da percursao da arvore
+    }));
+    if(btnSim.addEventListener("click", () => {
+        //faz outra pergunta
+    }))  
+}
